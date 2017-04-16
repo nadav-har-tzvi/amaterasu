1e02bc99ad775dbc213b5a0459668d83a0b299a2
Nadav Har Tzvi <nadavh@dealsumm.com>
2017-04-16 08:04:12 +0300

    added support for conda requirements list and requirement file in the dependencies list

--- leader/src/test/scala/io/shinto/amaterasu/leader/mesos/executors/DataLoaderTests.scala
+++ leader/src/test/scala/io/shinto/amaterasu/leader/mesos/executors/DataLoaderTests.scala
@@ -2,10 +2,7 @@ package io.shinto.amaterasu.leader.mesos.executors
 
 import java.nio.file.{Files, Paths}
 
-import io.shinto.amaterasu.common.execution.dependencies.{Artifact, Dependencies, PythonPackage, Repo}
 import org.scalatest.{FlatSpec, Matchers}
-
-import scala.collection.mutable.ListBuffer
 
 /**
   * Created by nadav-har-tzvi on 4/15/17.
@@ -16,19 +13,14 @@ class DataLoaderTests extends FlatSpec with Matchers {
     var url = getClass.getResource("/jars-only-conda1.yml")
     val path = url.getPath
     val dependencies = DataLoader.resolveDependencies(path)
-    dependencies.pythonPackages.head.length should not be 0
+    dependencies.condaPackages.length should not be 0
   }
 
-  "Multiple dependency files" should "be merged into one dependency file" in {
-    val dep1 = Dependencies(new ListBuffer[Repo], new ListBuffer[Artifact].toList, Option(new ListBuffer[PythonPackage].toList))
-    val pyPackages = new ListBuffer[PythonPackage]
-    pyPackages += PythonPackage("Django")
-    val dep2 = Dependencies(new ListBuffer[Repo], new ListBuffer[Artifact].toList, Option(pyPackages.toList))
-    val allDeps = new ListBuffer[Dependencies]
-    allDeps += dep1
-    allDeps += dep2
-    val mergedDeps = DataLoader.mergeDependencies(allDeps.toList)
-    mergedDeps.pythonPackages.head.head.packageId should be "Django"
+  "A parsed jars file with only conda req file" should "result in a dependencies that include a reqFile path" in {
+    var url = getClass.getResource("/jars-only-conda2.yml")
+    val path = url.getPath
+    val dependencies = DataLoader.resolveDependencies(path)
+    dependencies.condaReqFile.path should not be null
   }
 
 }
