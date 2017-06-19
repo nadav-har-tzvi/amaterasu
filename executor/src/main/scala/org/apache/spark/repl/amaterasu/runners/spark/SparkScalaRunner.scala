e1927ee9208c00adcb845703c63019df6ce358d8
Yaniv Rodenski <roadan@gmail.com>
2017-06-19 14:14:31 +1000

    all is YAML

--- executor/src/main/scala/org/apache/spark/repl/amaterasu/runners/spark/SparkScalaRunner.scala
+++ executor/src/main/scala/org/apache/spark/repl/amaterasu/runners/spark/SparkScalaRunner.scala
@@ -42,7 +42,6 @@ class SparkScalaRunner(var env: Environment,
 
     notifier.info(s"================= started action $actionName =================")
 
-
     for (line <- source.getLines()) {
 
       if (!line.isEmpty) {
