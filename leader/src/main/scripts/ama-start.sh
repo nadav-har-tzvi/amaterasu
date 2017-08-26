d55e52b0defe6054026af9dbeabbb661699e75bc
Yaniv Rodenski <roadan@gmail.com>
2017-08-26 16:52:10 +1000

    fixed string references to shointo

--- leader/src/main/scripts/ama-start.sh
+++ leader/src/main/scripts/ama-start.sh
@@ -20,7 +20,7 @@ BASEDIR=$(dirname "$0")
 
 export AMA_NODE="$(hostname)"
 #pushd $BASEDIR >/dev/null
-#cd /mesos-dependencies/ && nohup java -cp ${BASEDIR}/amaterasu-assembly-0.1.0.jar -Djava.library.path=/usr/lib io.shinto.amaterasu.utilities.HttpServer &
+#cd /mesos-dependencies/ && nohup java -cp ${BASEDIR}/amaterasu-assembly-0.1.0.jar -Djava.library.path=/usr/lib org.apache.amaterasu.utilities.HttpServer &
 #SERVER_PID=$!
 
 
@@ -53,7 +53,7 @@ echo "    / _ \ | '  \ / _\` ||  _|/ -_)| '_|/ _\` |(_-<| || | "
 echo "   /_/ \_\|_|_|_|\__,_| \__|\___||_|  \__,_|/__/ \_,_| "
 echo ""
 echo "    Continuously deployed data pipelines"
-echo "    Version 0.2.0"
+echo "    Version 0.2.0-incubating"
 echo "${NC}"
 echo ""
 
@@ -95,7 +95,7 @@ esac
 done
 
 echo "repo: ${REPO} "
-CMD="java -cp ${BASEDIR}/bin/*.jar -Djava.library.path=/usr/lib io.shinto.amaterasu.leader.mesos.JobLauncher --home ${BASEDIR}" #--repo "https://github.com/roadan/amaterasu-job-sample.git" --branch master
+CMD="java -cp ${BASEDIR}/bin/*-all.jar -Djava.library.path=/usr/lib org.apache.amaterasu.leader.mesos.JobLauncher --home ${BASEDIR}" #--repo "https://github.com/roadan/amaterasu-job-sample.git" --branch master
 
 if [ -n "$REPO" ]; then
     CMD+=" --repo ${REPO}"
@@ -123,12 +123,12 @@ fi
 
 if ! ls ${BASEDIR}/dist/spark*.tgz 1> /dev/null 2>&1; then
     echo "${bold} Fetching spark distributable ${NC}"
-    wget https://downloads.mesosphere.com/spark/assets/spark-2.1.0-1-bin-2.7.tgz -P ${BASEDIR}/dist
+    wget https://d3kbcqa49mib13.cloudfront.net/spark-2.1.1-bin-hadoop2.7.tgz -P ${BASEDIR}/dist
 fi
 
 echo $CMD
 
-eval $CMD #| grep "===>"
+eval $CMD | grep "===>"
 kill $SERVER_PID
 
 echo ""
